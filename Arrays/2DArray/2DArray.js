0b880d1dd992cf3fb5cd82c2e9fa5621d29112e0
Christopher Adams <chrismichaeladams@gmail.com>
2022-03-31 08:43:43 -0400

    feature(js):Start adding 2DArray problem description and boilerplate

---
+++ Arrays/2DArray/2DArray.js
@@ -0,0 +1,15 @@
+function hourglassSum(arr) {
+    // Write your code here
+    let inputArray = arr;
+    console.log("Input Array", inputArray);
+}
+
+let sampleArr = [
+    [1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0],
+    [0, 0, 2, 4, 4, 0],
+    [0, 0, 0, 2, 0, 0],
+    [0, 0, 1, 2, 4, 0],
+];
+hourglassSum(sampleArr);
