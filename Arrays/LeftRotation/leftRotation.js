efef047a27d0dfdf377fa8d5cf20c3694f5b9b76
Christopher Adams <chrismichaeladams@gmail.com>
2022-04-01 15:31:32 -0400

    Working Solution

--- Arrays/LeftRotation/leftRotation.js
+++ Arrays/LeftRotation/leftRotation.js
@@ -1,4 +1,15 @@
 function rotLeft(a, d) {
-    // Write your code here
+    const rotatedArray = [...a];
+    
+    for (let i = 0; i < d; i++) {
+    // Take off the first item from the array
+    let frontItem = rotatedArray.shift();
+    
+    // Push the item moved from the front to the back
+    rotatedArray.push(frontItem)
+    }
 
+    return rotatedArray;
 }
+
+rotageLeft([1, 2, 3, 4, 5], 4);
